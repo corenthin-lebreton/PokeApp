5d5ea513748cfa95631dc53a321e64f11120c563
firewax712 <firelol2003@gmail.com>
2022-12-13 23:24:35 +0100

    update(LoginForm): post to api + resolving bug get error message

--- poke-app/src/App.jsx
+++ poke-app/src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/home";
 import Login from "./Components/login";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/register" element={<Login />} />
+        <Route path="/accueil" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- poke-app/src/Components/login.jsx
+++ poke-app/src/Components/login.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/loginStyle.scss";
 import image from "../assets/logo.png";
 import { Button, Form } from "react-bootstrap";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleUsername = (e) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPassword = (e) => {
+    e.preventDefault();
+    setConfirmPassword(e.target.value);
+  };
+
+  const sendDataToApi = () => {
+    const data = {
+      username: username,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    try {
+      axios.post("http://localhost:3000/api/register", data).then((res) => {
+        console.log(res);
+        console.log(res.data);
+      });
+    } catch (error) {
+      //get error message from api
+      setError(error.response.data.message);
+    }
+  };
+
   return (
     <div>
       <img src={image} alt="logo" className="logo-login" />
@@ -17,6 +55,8 @@ const Login = () => {
                 className="form-fields"
                 type="string"
                 placeholder="nom d'utilisateur"
+                onChange={handleUsername}
+                required
               />
             </Form.Group>
             <Form.Group className="mb-3 test">
@@ -24,6 +64,8 @@ const Login = () => {
                 type="password"
                 placeholder="mot de passe"
                 className="form-fields"
+                onChange={handlePassword}
+                required
               />
             </Form.Group>
 
@@ -32,11 +74,14 @@ const Login = () => {
                 type="password"
                 placeholder="confirmer mot de passe"
                 className="form-fields"
+                onChange={handleConfirmPassword}
+                required
               />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary" onClick={sendDataToApi}>
               Connexion
             </Button>
+            {error ? <p className="error">{error}</p> : null}
           </Form>
         </div>
       </div>
